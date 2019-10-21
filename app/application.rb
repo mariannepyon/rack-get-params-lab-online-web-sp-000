063958da425b65c153ac7af60bc87f76ebd60522
Marianne Pyon <mariannepyon@gmail.com>
2019-10-21 18:39:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,13 +7,20 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    if req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |cart_item|
+          resp.write "#{cart_item}\n"
+    elsif req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+    
     else
       resp.write "Path Not Found"
     end
