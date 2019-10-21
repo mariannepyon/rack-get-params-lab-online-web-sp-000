5a0381368105bf6cf3db5a6195a9e1b0a8bdccc2
Marianne Pyon <mariannepyon@gmail.com>
2019-10-21 18:14:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,51 +3,29 @@ class Application
   @@items = ["Apples","Carrots","Pears"]
   @@cart = []
 
-    def call(env)
-      resp = Rack::Response.new
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
-      elsif req.path.match(/search/)
-        search_term = req.params["q"]
-        resp.write handle_search(search_term)
-      elsif req.path.match(/cart/)
-        resp.write @@cart.empty? ? "Your cart is empty" : @@cart.join("\n")
-      elsif req.path.match(/add/)
-        item = req.params["item"]
-        if @@items.include?(item)
-          @@cart << item
-          resp.write "added #{item}"
-        else
-          resp.write "We don't have that item"
-        end
-      else
-        resp.write "Path Not Found"
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
       end
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    else
+      resp.write "Path Not Found"
+    end
 
+    resp.finish
+  end
 
-
-#   def call(env)
-#     resp = Rack::Response.new
-#     req = Rack::Request.new(env)
-#
-#     if req.path.match(/items/)
-#       @@items.each do |item|
-#         resp.write "#{item}\n"
-#       end
-#     elsif req.path.match(/search/)
-#       search_term = req.params["q"]
-#       resp.write handle_search(search_term)
-#     else
-#       resp.write "Path Not Found"
-#     end
-#
-#     resp.finish
-#   end
-#
-#   def handle_search(search_term)
-#     if @@items.include?(search_term)
-#       return "#{search_term} is one of our items"
-#     else
-#       return "Couldn't find #{search_term}"
-#     end
-#   end
-# end
+  def handle_search(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
+end
