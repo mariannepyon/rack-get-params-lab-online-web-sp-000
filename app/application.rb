5ace41f71e6a1b3dfc25ec90850305744dbd6516
Marianne Pyon <mariannepyon@gmail.com>
2019-10-21 18:27:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -21,22 +21,22 @@ class Application
   #   resp.finish
   # end
 
-elsif req.path.match(/search/)
-     search_term = req.params["q"]
-     resp.write handle_search(search_term)
-   elsif req.path.match(/cart/)
-     resp.write @@cart.empty? ? "Your cart is empty" : @@cart.join("\n")
-   elsif req.path.match(/add/)
-     item = req.params["item"]
-     if @@items.include?(item)
-       @@cart << item
-       resp.write "added #{item}"
-     else
-       resp.write "We don't have that item"
-     end
-   else	    
-     resp.write "Path Not Found"
-   end
+    elsif req.path.match(/search/)
+         search_term = req.params["q"]
+         resp.write handle_search(search_term)
+       elsif req.path.match(/cart/)
+         resp.write @@cart.empty? ? "Your cart is empty" : @@cart.join("\n")
+       elsif req.path.match(/add/)
+         item = req.params["item"]
+         if @@items.include?(item)
+           @@cart << item
+           resp.write "added #{item}"
+         else
+           resp.write "We don't have that item"
+         end
+       else	    
+         resp.write "Path Not Found"
+       end
 
   def handle_search(search_term)
     if @@items.include?(search_term)
