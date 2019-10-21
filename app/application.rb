dedcf6745257ccbdfb48ac72aaa59043eda0058f
Marianne Pyon <mariannepyon@gmail.com>
2019-10-21 18:13:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,37 +3,32 @@ class Application
   @@items = ["Apples","Carrots","Pears"]
   @@cart = []
 
-  def call(env)
-     resp = Rack::Response.new	    resp = Rack::Response.new
- @@ -13,6 +14,22 @@ def call(env)
-     elsif req.path.match(/search/)	    elsif req.path.match(/search/)
-       search_term = req.params["q"]	      search_term = req.params["q"]
-       resp.write handle_search(search_term)	      resp.write handle_search(search_term)
-     elsif req.path.match(/cart/)
-       if @@cart.empty?
-         resp.write "Your cart is empty"
-       else
-         @@cart.each do |cart_item|
-           resp.write "#{cart_item}\n"
-         end
-       end
-     elsif req.path.match(/add/)
-       item = req.params["item"]
-       if @@items.include?(item)
-         @@cart << item
-         resp.write "added #{item}"
-       else
-         resp.write "We don't have that item"
-       end
-    else
-      resp.write "Path Not Found"
-    end
-  end
-end
+    def call(env)
+      resp = Rack::Response.new
+      
+      elsif req.path.match(/search/)	    
+        search_term = req.params["q"]
+        resp.write handle_search(search_term)
+      elsif req.path.match(/cart/)
+        resp.write @@cart.empty? ? "Your cart is empty" : @@cart.join("\n")
+      elsif req.path.match(/add/)
+        item = req.params["item"]
+        if @@items.include?(item)
+          @@cart << item
+          resp.write "added #{item}"
+        else
+          resp.write "We don't have that item"
+        end
+      else 
+        resp.write "Path Not Found"
+      end
+
+
+
 #   def call(env)
 #     resp = Rack::Response.new
 #     req = Rack::Request.new(env)
-#
+# 
 #     if req.path.match(/items/)
 #       @@items.each do |item|
 #         resp.write "#{item}\n"
@@ -44,10 +39,10 @@ end
 #     else
 #       resp.write "Path Not Found"
 #     end
-#
+# 
 #     resp.finish
 #   end
-#
+# 
 #   def handle_search(search_term)
 #     if @@items.include?(search_term)
 #       return "#{search_term} is one of our items"
